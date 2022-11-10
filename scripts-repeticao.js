081b214a5d927af2913f2ef512969c86610719fd
neto <euneto2501@gmail.com>
2022-11-10 00:38:02 -0300

    aula 18 finalizada

---
+++ scripts-repeticao.js
@@ -0,0 +1,40 @@
+//For
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i) 
+}
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i) 
+}
+
+//While
+let interator = 1
+while (interator <= 20) {
+  console.log(interator)
+  
+  interator++
+}
+
+//For OF
+let listNames = ['Neto', 'Camilla', 'Sophia', 'Dante']
+for (names of listNames) {
+  console.log(names)
+}
+
+//For in
+let animal = {
+  name: 'leão',
+  weight: 200,
+  age: 4
+}
+for(item in animal) {
+  console.log(animal[item])
+}
+
+//For in com array
+let myFamily = ['Neto', 'Camilla', 'Sophia', 'Dante']
+
+for (const item in myFamily) {
+  console.log(myFamily[item])
+}
